a3ffb08edfd94381f8cd885daf63ea652c2f6f6d
coolnsmile <coolnsmile@naver.com>
2019-03-23 13:07:08 +0900

    commit

---
+++ deploy.bat
@@ -0,0 +1,1 @@
+java -jar -Xms512m -Xmx512m -Djava.security.egd=file:/dev/./urandom ./target/was2-0.0.1-SNAPSHOT.jar
--- deploy2.sh
+++
@@ -1,3 +0,0 @@
-rm mylog.txt
-rm nohup.out
-nohup java -jar -Xms512m -Xmx512m -Djava.security.egd=file:/dev/./urandom ./target/was2-0.0.1-SNAPSHOT.jar &
